8ad5aafe28e6e7250b11aabe6e90b614c93b8b62
saksonobayu <saksonobayuaji@gmail.com>
2022-06-24 17:52:01 +0700

    first commit

---
+++ app/src/main/java/com/big/loginsignup/BookMovie.kt
@@ -0,0 +1,11 @@
+package com.big.loginsignup
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class BookMovie : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_book_movie)
+    }
+}
---
+++ app/src/main/java/com/big/loginsignup/SeatMovie.kt
@@ -0,0 +1,11 @@
+package com.big.loginsignup
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SeatMovie : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_seat_movie)
+    }
+}
